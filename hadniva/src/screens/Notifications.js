dd564fe6ddca031e8e69be43843627a70866e99e
Josephus67 <jbawah002@st.ug.edu.gh>
2024-07-27 14:47:23 -0700

    no notification screen

--- hadniva/src/screens/Notifications.js
+++ hadniva/src/screens/Notifications.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const Notifications = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is Notifications Screen</Text>
+      <Image source={require('../asset/NoNotification.png')} />
+      <View style={styles.NoNotification}>
+      <Icon name="notifications-off" size={30} color="black" />
+      <Text style={styles.text}>You have No New Notifications</Text>
+      </View>
     </View>
   );
 };
@@ -15,11 +19,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    //backgroundColor: 'grey',
   },
   text: {
     fontSize: 18,
   },
-
+NoNotification: {
+  flexDirection: 'row',
+  gap: 10
+}
 });
 
 export default Notifications;
